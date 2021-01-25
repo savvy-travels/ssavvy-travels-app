4c2ca490bc1ee1915cd89d04d4164ebc260f2db3
Andrew Turner <turner.andrew0@gmail.com>
2021-01-25 11:09:24 -0600

    placed api key in .env

--- src/components/Apisetup.js
+++ src/components/Apisetup.js
@@ -8,10 +8,12 @@ const Apisetup = () => {
     const [places, setPlaces] = useState([])
     const [carriers, setCarriers] = useState([])
 
+    const { REACT_APP_CLIENT_ID} = process.env
+
     useEffect(() => {
         axios.get(`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/DFW-sky/anywhere/2021-02-05/2021-02-08`, {
             headers: {
-                'x-rapidapi-key': `293c8f1306mshd1179b84f5495fdp1624a6jsn253fcf20a6a7`
+                'x-rapidapi-key': `${REACT_APP_CLIENT_ID}`
             }
         })
         .then
@@ -21,14 +23,14 @@ const Apisetup = () => {
             setCarriers(res.data.Carriers)
         })
     }, [])
-
+    
     const flights = quotes.map((quote) => {
         let destinationId = places.findIndex(place => place.PlaceId === quote.OutboundLeg.DestinationId)
         let carrierId = carriers.findIndex(carrier => carrier.CarrierId === quote.OutboundLeg.CarrierIds)
-
+        
         return {...quote, ...places[destinationId], ...carriers[carrierId]}
     })
-
+    
     const flightCards = flights.map((flight) => {
         return (
             <div key={flight.QuoteId} className='flight-card'>
@@ -37,10 +39,10 @@ const Apisetup = () => {
             </div>
         )
     })
-
+    
     return (
         
-        <main>
+        <main className='api'>
             <h1>Api Results</h1>
             <div className='flight-results'>{flightCards}</div>
            
