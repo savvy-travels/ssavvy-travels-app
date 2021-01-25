5bab8e0c1cc9982bb402ea734841f3cc51a82d9b
Nathan Waite <natew821@gmail.com>
2021-01-25 16:08:47 -0700

    Styling on signup

--- src/components/Landing/Auth/Signup.js
+++ src/components/Landing/Auth/Signup.js
@@ -2,19 +2,19 @@ import React from 'react'
 import { withRouter } from 'react-router-dom'
 import './auth.css'
 
-function Signup(props){
-    return(
-        <span className='search-field'>
-            <div className='input-field'>
+function Signup(props) {
+    return (
+        <span className='register-field'>
+            <div className='register-input-field'>
                 <div className='register-header'>
                     <h2>Register</h2>
                     <h4>Already a member?<h4>Login</h4></h4>
                 </div>
-                <input className='register-inputs' type='email' placeholder='email'/>
-                <input className='register-inputs' type='text' placeholder='Preferred Airport'/>
-                <input className='register-inputs' type='password' placeholder='Password'/>
-                <input className='register-inputs' type='password' placeholder='Confirm Password'/>
-            <button className='search-button'>Signup</button>
+                <input className='register-inputs' type='email' placeholder='Email' />
+                <input className='register-inputs' type='text' placeholder='Preferred Airport' />
+                <input className='register-inputs' type='password' placeholder='Password' />
+                <input className='register-inputs' type='password' placeholder='Confirm Password' />
+                <button className='search-button'>Signup</button>
             </div>
         </span>
     )
