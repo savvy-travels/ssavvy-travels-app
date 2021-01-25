c17c5d6c002778777302820a854f795c57ac32a1
Nathan Waite <natew821@gmail.com>
2021-01-25 16:41:32 -0700

    Set Up Front End Logic, waiting on back

--- src/components/Landing/Auth/Signup.js
+++ src/components/Landing/Auth/Signup.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 import { Link, withRouter } from 'react-router-dom'
 import './auth.css'
 
 function Signup(props) {
+    const [email, setEmail] = useState('')
+    const [username, setUsername] = useState('')
+    const [airport, setAirport] = useState('SLC')
+    const [password, setPassword] = useState('')
+    const [confirmPass, setConfirmPass] = useState('')
+    const [loading, setLoading] = useState(false)
+
+
+    function registerUser() {
+        props.history.push('/')
+        console.log(email, username, airport, password)
+        // axios.post('/api/auth/register', { email, username, password, airport }).then(res => {
+        //     console.log(res.data)
+        // })
+    }
+
     return (
         <span className='register-field'>
             <div className='register-input-field'>
@@ -10,11 +27,27 @@ function Signup(props) {
                     <h2>Register</h2>
                     <h4>Already a member?<h4><Link style={{ textDecoration: 'none' }} to='/login'>Login</Link></h4></h4>
                 </div>
-                <input className='register-inputs' type='email' placeholder='Email' />
-                <input className='register-inputs' type='text' placeholder='Preferred Airport' />
-                <input className='register-inputs' type='password' placeholder='Password' />
-                <input className='register-inputs' type='password' placeholder='Confirm Password' />
-                <button className='search-button'>Signup</button>
+                <input onChange={(e) => setEmail(e.target.value)}
+                    className='register-inputs'
+                    type='email'
+                    placeholder='Email' />
+                <input onChange={(e) => setUsername(e.target.value)}
+                    className='register-inputs'
+                    type='email'
+                    placeholder='Username' />
+                <input onChange={(e) => setAirport(e.target.value)}
+                    className='register-inputs'
+                    type='text'
+                    placeholder='Preferred Airport' />
+                <input onChange={(e) => setPassword(e.target.value)}
+                    className='register-inputs'
+                    type='password'
+                    placeholder='Password' />
+                <input onChange={(e) => setConfirmPass(e.target.value)}
+                    className='register-inputs'
+                    type='password'
+                    placeholder='Confirm Password' />
+                <button onClick={() => registerUser()} className='search-button'>Signup</button>
             </div>
         </span>
     )
