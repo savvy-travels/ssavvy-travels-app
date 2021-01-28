802f7a2b9bc9b3d436095a9548d6b6e26408b06d
Josh Friedel <joshfriedel95@gmail.com>
2021-01-28 11:27:51 -0600

    Fixed possibly maybe

--- server/controllers/nodemailer.js
+++ server/controllers/nodemailer.js
@@ -1,21 +1,21 @@
-module.exports = {
-    sendEmail: async(req,res) => {
-        const {message, title, email} = req.body
-                const transporter = await req.app.get('transporter')
-                transporter.sendMail({
-                    from: 'savvytravels11@gmail.com',
-                    to: email,
-                    subject: title,
-                    text: message
-                },
-                function(err, info){
-                    if(err){
-                        console.log(err)
-                    }else {
-                        console.log('email sent:' + info.response)
-                    }
-                })
-                res.sendStatus(200)
-    }
+// module.exports = {
+//     sendEmail: async(req,res) => {
+//         const {message, title, email} = req.body
+//                 const transporter = await req.app.get('transporter')
+//                 transporter.sendMail({
+//                     from: 'savvytravels11@gmail.com',
+//                     to: email,
+//                     subject: title,
+//                     text: message
+//                 },
+//                 function(err, info){
+//                     if(err){
+//                         console.log(err)
+//                     }else {
+//                         console.log('email sent:' + info.response)
+//                     }
+//                 })
+//                 res.sendStatus(200)
+//     }
 
-}
+// }
