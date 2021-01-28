55fe4ed711b2491247107a26763b36f3992ec8c6
Josh Friedel <joshfriedel95@gmail.com>
2021-01-28 13:23:46 -0600

    Deleted old code

--- src/components/Landing/Auth/Signup.js
+++ src/components/Landing/Auth/Signup.js
@@ -21,14 +21,7 @@ function Signup(props) {
     const [passError, setPassError] = useState(false)
     const [error, setError] = useState(false)
 
-// Write function to pass back information here
-    // const  Confirmation = () => {
-    //     const message = `Confirmation ${username}`
-    //     const title = 'Confirmation'
-    //     axios.post('/api/confirmation', {message, title, email}).then(() => {
-    //         console.log('Email Sent')
-    //     }).catch((err) => console.log(err))
-    // }
+
 
     function registerUser() {
         setPassError(false)
@@ -44,9 +37,9 @@ function Signup(props) {
         setLoading(true)
         const message = `Confirmation ${username}`
         const title = 'Confirmation'
-        axios.post('/api/auth/register', { email, username, password, preferred, message, title }).then(res => {
+        // const image = 'https://colab-image-assets.s3-us-west-1.amazonaws.com/Savvy-Travels-logo.png'
+        axios.post('/api/auth/register', { email, username, password, preferred, message, title}).then(res => {
             console.log('Hit this')
-            // Confirmation()
             setLoading(false)
             setError(false)
             props.loginUser(res.data)
