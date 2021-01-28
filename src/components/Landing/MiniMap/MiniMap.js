d2806da8ed2441af4b3b8ea7efa012add4641274
nickloverde <nick.loverde@gmail.com>
2021-01-28 12:33:27 -0700

    resolved map not resizing

--- src/components/Landing/MiniMap/MiniMap.js
+++ src/components/Landing/MiniMap/MiniMap.js
@@ -15,6 +15,22 @@ function MiniMap(props) {
     })
     const [selectedCity, setSelectedCity] = useState(null)
 
+    React.useEffect(() => {
+        window.addEventListener('resize', () => {
+            setViewport({
+                latitude: lat,
+                longitude: long,
+                width: '100%',
+                height: '100%',
+                zoom: 3
+            })
+        })
+
+        return () => {
+            window.removeEventListener('resize')
+        }
+    }, [])
+
     //Search State//
     // const [budget, setBudget] = useState(`$${props.match.params.budget}`)
     // const [where, setWhere] = useState(props.match.params.where)
