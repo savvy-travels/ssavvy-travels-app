4f35aab2a0247e4ec7f1ecf52b74d7c949235f37
nickloverde <nick.loverde@gmail.com>
2021-02-02 13:36:50 -0700

    fixed marker css

--- src/components/Landing/MiniMap/MiniMap.js
+++ src/components/Landing/MiniMap/MiniMap.js
@@ -1,6 +1,6 @@
 import React, { useState, useMemo, useEffect } from 'react'
 import { withRouter } from 'react-router-dom'
-import ReactMapGL, { Marker, Popup } from 'react-map-gl'
+import ReactMapGL, { Marker} from 'react-map-gl'
 import './minimap.css'
 
 function MiniMap(props) {
@@ -16,19 +16,19 @@ function MiniMap(props) {
     zoom: 3,
   })
 
-  const suggested = [flights[0], flights[1], flights[2]]
+//   const suggested = [flights[0], flights[1], flights[2]]
 
-  const suggestedCards = suggested.map(flight => (
-    <div key={flight.QuoteId} className='flight-card'>
-      <h3>{flight.CityName}</h3>
-      <h1>${flight.MinPrice}</h1>
-    </div>
-  ))
+//   const suggestedCards = suggested.map(flight => (
+//     <div key={flight.QuoteId} className='flight-card'>
+//       <h3>{flight.CityName}</h3>
+//       <h1>${flight.MinPrice}</h1>
+//     </div>
+//   ))
 
   const markers = useMemo(() => flights.map(
     city => (
         <div>{city.lon ? 
-        <Marker 
+        <Marker
         key={city.CityName} 
         longitude={+city.lon} 
         latitude={+city.lat} 
@@ -69,46 +69,48 @@ function MiniMap(props) {
     <div className='mini-map-container'>
       <div className='mini-map-side-bar'>
         <div className='suggestion-title'>
-        {selectedCity ? (
+            {/* {flights.QuoteId ? 
+                <div>
+                <h1>Suggested Trips</h1>
+                {<div>{suggestedCards}</div>}
+                <h5 className='map-click'>Click on the map icons to see details</h5>
+                </div>
+                : null} */}
+
+            {selectedCity ? (
                 <div className='popup'>
                     <img className= 'popup-img' src='https://upload.wikimedia.org/wikipedia/commons/3/32/20190616154621%21Echo_Park_Lake_with_Downtown_Los_Angeles_Skyline.jpg'/>
                     <h2>City: {selectedCity.CityName}</h2>
                     <h3>Price: ${selectedCity.MinPrice}</h3>
                     <h4>{(selectedCity.Direct) ? 'Direct' : 'Multiple-stops'
                         }</h4>
-                    <button className='search-button'>Go to Flight</button>
                     <button
                         // onClick= {isLoggedIn ? saveLocation to profile : Link to register page>Add to favorites}
                     >
                         {/* <img src='plus-icon'/> */}
                     </button>
+                    <button className='search-button'>Go to Flight</button>
                 </div>
-            // </Popup>
-         ) : null}
-
-          <h1>Suggested Trips</h1>
-          {<div>{suggestedCards}</div>}
+            ) : null}
 
         </div>
       </div>
 
       <div className='mini-map'>
-        <ReactMapGL
-          {...viewport}
-          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-          mapStyle='mapbox://styles/nickloverde/ckkew55if03e817o5o2je6rkp'
-          //allows us to drag map around and zoom in/out
-          onViewportChange={(viewport) => {
-            setViewport({ ...viewport })
-          }}
-        >
+            <ReactMapGL
+            {...viewport}
+            mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+            mapStyle='mapbox://styles/nickloverde/ckkew55if03e817o5o2je6rkp'
+            //allows us to drag map around and zoom in/out
+            onViewportChange={(viewport) => {
+                setViewport({ ...viewport })
+            }}
+            >
+                {/* Markers */}
+                {markers}
         
-        {/* Markers */}
-        {markers}
-      
-        </ReactMapGL>
+            </ReactMapGL>
       </div>
-
 
     </div>
 
