8a4af893ff9ac079f9f8d192b096b8d525565beb
Josh Friedel <joshfriedel95@gmail.com>
2021-02-02 11:56:56 -0600

    Styling

--- src/components/Landing/Works/Works.js
+++ src/components/Landing/Works/Works.js
@@ -3,22 +3,22 @@ import './work.scss'
 function Work() {
     return (
         <div className='work-container'>
-            <div>
+            <div className='mini-container'>
                 <div>Picture 1</div>
                 <h1>Give Us the Budget</h1>
                 <div>Tell us how much you want to spend and customize your departure and arrival dates.</div>
             </div>
-            <div>
+            <div className='mini-container'>
                 <div>Picture 2</div>
                 <h1>Explore the Map</h1>
-                <div>Look at our map and you'll be surprised with how far you can go.</div>
+                <div>Look at our map and you'll be surprised how far you can go.</div>
             </div>
-            <div>
+            <div className='mini-container'>
                 <div>Picture 3</div>
                 <h1>See Your Best Flights</h1>
                 <div>We will show you the cheapest flight we found from comparing thousands of flights.</div>
             </div>
-            <div>
+            <div className='mini-container'>
                 <div>Picture 4</div>
                 <h1>Book the Lowest Prices</h1>
                 <div>Book your flight! We guarantee to get you the cheapest possible flight or your money back guaranteed</div>
