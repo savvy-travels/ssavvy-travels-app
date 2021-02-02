01880f2a6dc1710db8ea96ca4dd7f7c74d3050d2
Josh Friedel <joshfriedel95@gmail.com>
2021-02-02 13:28:01 -0600

    More styling

--- src/components/Landing/Works/Works.js
+++ src/components/Landing/Works/Works.js
@@ -3,25 +3,28 @@ import './work.scss'
 function Work() {
     return (
         <div className='work-container'>
-            <div className='mini-container-budget'>
+            <div className='mini-container'>
                 <img src='https://abcsofsellingonline.com/wp-content/uploads/2020/06/give-money.png' alt='Budget' />
                 <h1>Give Us Your Budget</h1>
                 <div>Tell us how much you want to spend and customize your departure and arrival dates.</div>
             </div>
+            <img className='arrow' src='https://cdn.discordapp.com/attachments/801884180879376405/806242899720404992/arrow-40163_960_720.webp' alt='Arrow'/> 
             <div className='mini-container'>
                 <img src='https://img2.pngio.com/free-road-map-png-free-road-mappng-transparent-images-22681-road-map-icon-png-980_998.png' alt='Map'/>
                 <h1>Explore the Map & Destinations</h1>
                 <div>Look at our map and you will be surprised how far your budget can take you.</div>
             </div>
+            <img className='arrow' src='https://cdn.discordapp.com/attachments/801884180879376405/806242899720404992/arrow-40163_960_720.webp' alt='Arrow'/> 
             <div className='mini-container'>
                 <img src='https://lh3.googleusercontent.com/proxy/9o-3RxZTLBiewdIfGNVVpB2A60lBevC9hW7aBZJBRqGvBnpQlqa_vGsrgwrqpXiuyMIa5WmtGOr4jEVlAfI8SrWUCs0fI3xzCbMIteqh-g' alt='Plane'/>
                 <h1>See Your Best Flights</h1>
                 <div>We will show you the cheapest flight we found from comparing thousands of flights.</div>
             </div>
+            <img className='arrow' src='https://cdn.discordapp.com/attachments/801884180879376405/806242899720404992/arrow-40163_960_720.webp' alt='Arrow'/> 
             <div className='mini-container'>
                 <img src='https://image.flaticon.com/icons/png/512/85/85536.png' alt='Book'/>
                 <h1>Book the Lowest Prices</h1>
-                <div>Book your flight! We guarantee to get you the cheapest possible flight or your money back guaranteed.</div>
+                <div>Book your flight! We guarantee to get you the cheapest possible flight.</div>
             </div>
         </div>
     )
