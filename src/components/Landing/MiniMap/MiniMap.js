4c7a862e7a029d2a512ebf5b4219a63617d2ac83
Andrew Turner <turner.andrew0@gmail.com>
2021-02-04 16:17:34 -0600

    go to flight button works on minimap

--- src/components/Landing/MiniMap/MiniMap.js
+++ src/components/Landing/MiniMap/MiniMap.js
@@ -96,6 +96,107 @@ useEffect(()=>{
     top: 10
   };
 
+
+  const goToCarrier = (carrier) => {
+    switch(carrier) {
+      case 'Gulf Air':
+        window.location.href='https://www.gulfair.com/'
+      break;
+      case 'Oman Air':
+        window.location.href='https://www.omanair.com/cn/en'
+      break;
+      case 'Qatar Airways':
+        window.location.href='https://www.qatarairways.com/en-us/homepage.html'
+      break;
+      case 'Sun Country Airlines':
+        window.location.href='https://suncountry.com/'
+      break;
+      case 'Qantas':
+        window.location.href='https://www.qantas.com/us/en.html'
+      break;
+      case 'Volaris':
+        window.location.href='https://www.volaris.com/'
+      break;
+      case 'French Bee':
+        window.location.href='https://www.frenchbee.com/'
+      break;
+      case 'GOL Linhas Aéreas':
+        window.location.href='https://www.voegol.com.br/en'
+      break;
+      case 'Spirit Airlines':
+        window.location.href='https://www.spirit.com/'
+      break;
+      case 'Alaska Airlines':
+        window.location.href='https://www.alaskaair.com/'
+      break;
+      case 'Lufthansa':
+        window.location.href='https://www.lufthansa.com/us/en/homepage'
+      break;
+      case 'Avianca':
+        window.location.href='https://www.avianca.com/us/en/'
+      break;
+      case 'Emirates':
+        window.location.href='https://www.emirates.com/us/english/'
+      break;
+      case 'Aeromexico':
+        window.location.href='https://aeromexico.com/en-us'
+      break;
+      case 'SAS':
+        window.location.href='https://www.flysas.com/us-en/'
+      break;
+      case 'Iberia':
+        window.location.href='https://www.iberia.com/us/'
+      break;
+      case 'Brussels Airlines':
+        window.location.href='https://www.brusselsairlines.com/'
+      break;
+      case 'British Airways':
+        window.location.href='https://www.britishairways.com/travel/home/public/en_us'
+      break;
+      case 'Frontier Airlines':
+        window.location.href='https://www.flyfrontier.com/'
+      break;
+      case 'United':
+        window.location.href='https://www.united.com/en/us'
+      break;
+      case 'jetBlue':
+        window.location.href='https://www.jetblue.com/'
+      break;
+      case 'Turkish Airlines':
+        window.location.href='https://www.turkishairlines.com/'
+      break;
+      case 'Air Canada':
+        window.location.href='https://www.aircanada.com/us/en/aco/home.html'
+      break;
+      case 'Japan Airlines':
+        window.location.href='https://www.jal.co.jp/ar/en/'
+      break;
+      case 'China Air':
+        window.location.href='https://www.china-airlines.com/us/en'
+      break;
+      case 'Finnair':
+        window.location.href='https://www.finnair.com/'
+      break;
+      case 'Korean Air':
+        window.location.href='https://www.koreanair.com/us/en'
+      break;
+      case 'Air France':
+        window.location.href='https://www.airfrance.com/indexCom_en.html'
+      break; 
+      case 'Ethiopian Airlines':
+        window.location.href='https://www.ethiopianairlines.com/us'
+      break; 
+      case 'Copa':
+        window.location.href='https://www.copaair.com/en/web/us'
+      break; 
+      case 'Iceland Air':
+        window.location.href='https://www.icelandair.com/'
+      break; 
+      default:
+       window.location.href='/'
+    }
+  }
+
   return (
     <div className='mini-map-container'>
       <div className='mini-map-side-bar'>
@@ -103,13 +204,13 @@ useEffect(()=>{
           {selectedCity ? (
             <div className='popup'>
 
-              <img className='popup-img' src='https://assets.cairo360.com/app/uploads/2019/01/getty_583734066_335273.jpg' />
+              <img className='popup-img' src='https://assets.cairo360.com/app/uploads/2019/01/getty_583734066_335273.jpg' alt='' />
 
               <h2>City: {selectedCity.CityName}</h2>
               <h3>Price: ${selectedCity.MinPrice}</h3>
               <h4>{(selectedCity.Direct) ? 'Direct' : 'Multiple-stops'
               }</h4>
-              <button className='search-button'>Go to Flight</button>
+              <button className='search-button' onClick={() => goToCarrier(selectedCity.Name)}>Go to Flight</button>
             </div>
           ) : null}
           <h1 className='suggested-header'>Suggested Trips</h1>
