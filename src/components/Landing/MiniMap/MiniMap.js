8c59debe159facf1aff6ad5865e1ee618eb3f49c
Nathan Waite <natew821@gmail.com>
2021-02-05 09:29:26 -0700

    Fixed Errors

--- src/components/Landing/MiniMap/MiniMap.js
+++ src/components/Landing/MiniMap/MiniMap.js
@@ -1,29 +1,29 @@
-import React, { useState, useMemo, useEffect, useContext } from 'react'
-import { withRouter } from 'react-router-dom'
-import ReactMapGL, { Marker, GeolocateControl } from 'react-map-gl'
-import moment from 'moment'
-import './minimap.css'
-import { Context } from '../../../context/context'
-const photos = require('../../../photos.json')
+import React, { useState, useMemo, useEffect, useContext } from "react";
+import { withRouter } from "react-router-dom";
+import ReactMapGL, { Marker, GeolocateControl } from "react-map-gl";
+import moment from "moment";
+import "./minimap.css";
+import { Context } from "../../../context/context";
+const photos = require("../../../photos.json");
 
 function MiniMap(props) {
-  const context = useContext(Context)
-  
-const [suggestedCards, setSuggestedCards] = useState([])
+  const context = useContext(Context);
+
+  const [suggestedCards, setSuggestedCards] = useState([]);
   //Map State
-  const { lat, long, flights } = props
-  const [selectedCity, setSelectedCity] = useState(null)
+  const { lat, long, flights } = props;
+  const [selectedCity, setSelectedCity] = useState(null);
   const [viewport, setViewport] = useState({
     latitude: context.lat,
     longitude: context.long,
     width: "100%",
     height: "100%",
     zoom: 3,
-  })
+  });
 
   //Liked Trip//
 
-  const suggested = flights.slice(0, 10)
+  const suggested = flights.slice(0, 10);
 
   useEffect(() => {
     setSuggestedCards(
@@ -97,35 +97,10 @@ const [suggestedCards, setSuggestedCards] = useState([])
               </div>
             </Marker>
           ) : null}
-      </div>
-     )
-    }
-  )
-)},[flights])
-
-  const markers = useMemo(() => flights.map(
-    city => (
-      <div>{city.lon ?
-        <Marker
-          key={city.CityName}
-          longitude={+city.lon}
-          latitude={+city.lat}
-          className='marker'>
-          <div className='marker-container'>
-            <button
-              onClick={e => {
-                e.preventDefault()
-                setSelectedCity(city)
-              }}
-              className='marker-btn'>
-              <p className='price'>${city.MinPrice}</p>
-              <img className='marker-icon' src='https://cdn4.iconfinder.com/data/icons/basic-ui-pack-flat-s94-1/64/Basic_UI_Icon_Pack_-_Flat_map_pointer-512.png'/>
-            </button>
-          </div>
-        </Marker> : null}
-      </div>
-    )
-  ), [flights]);
+        </div>
+      )),
+    [flights]
+  );
 
   useEffect(() => {
     window.addEventListener("resize", () => {
@@ -135,72 +110,69 @@ const [suggestedCards, setSuggestedCards] = useState([])
         width: "100%",
         height: "100%",
         zoom: 3,
-      })
-    })
+      });
+    });
     return () => {
-      window.removeEventListener("resize", setViewport)
-    }
-  }, [])
+      window.removeEventListener("resize", setViewport);
+    };
+  }, []);
 
   const geolocateControlStyle = {
     right: 10,
-    top: 10
+    top: 10,
   };
 
-
-  
-
   return (
-    <div className='mini-map-container'>
-      <div className='mini-map-side-bar'>
-        <div className='suggestion-title'>
+    <div className="mini-map-container">
+      <div className="mini-map-side-bar">
+        <div className="suggestion-title">
           {selectedCity ? (
             <di>
-            <div className='sticky-marker'>
-              <div
-                key={selectedCity.QuoteId}
-                className="map-flight-card-selected"
-              >
-                <span className="map-image-container">
-                  <img
-                    className="map-flight-card-image"
-                    src="https://i.pinimg.com/originals/08/1f/0a/081f0a864808d6efc0883014e802bc25.jpg"
-                  />
-                </span>
-                <span className="map-info-container">
-                  <div className="selected-map-info-div">
-                    <h1>{selectedCity.CityName}</h1>
-                    <h4>
-                      {moment(
-                        selectedCity.OutboundLeg.DepartureDate
-                      ).format("MMM Do YYYY")}
-                    </h4>
-                    <h4>{`${selectedCity.Direct ? "Direct - " : ""}${
-                      selectedCity.name
-                    }`}</h4>
-                  </div>
-                  <h1>
-                    <h6>From</h6> ${selectedCity.MinPrice}
-                  </h1>
-                </span>
+              <div className="sticky-marker">
+                <div
+                  key={selectedCity.QuoteId}
+                  className="map-flight-card-selected"
+                >
+                  <span className="map-image-container">
+                    <img
+                      className="map-flight-card-image"
+                      src="https://i.pinimg.com/originals/08/1f/0a/081f0a864808d6efc0883014e802bc25.jpg"
+                    />
+                  </span>
+                  <span className="map-info-container">
+                    <div className="selected-map-info-div">
+                      <h1>{selectedCity.CityName}</h1>
+                      <h4>
+                        {moment(selectedCity.OutboundLeg.DepartureDate).format(
+                          "MMM Do YYYY"
+                        )}
+                      </h4>
+                      <h4>{`${selectedCity.Direct ? "Direct - " : ""}${
+                        selectedCity.name
+                      }`}</h4>
+                    </div>
+                    <h1>
+                      <h6>From</h6> ${selectedCity.MinPrice}
+                    </h1>
+                  </span>
+                </div>
               </div>
-            </div>
-            <div className="mini-map-line"></div>
-          </di>
+              <div className="mini-map-line"></div>
+            </di>
           ) : null}
-          <h1 className='suggested-header'>Suggested Trips</h1>
+          <h1 className="suggested-header">Suggested Trips</h1>
           {<div>{suggestedCards}</div>}
         </div>
       </div>
 
-      <div className='mini-map'>
+      <div className="mini-map">
         <ReactMapGL
           {...viewport}
           mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-          mapStyle='mapbox://styles/nickloverde/ckkew55if03e817o5o2je6rkp'
+          mapStyle="mapbox://styles/nickloverde/ckkew55if03e817o5o2je6rkp"
           //allows us to drag map around and zoom in/out
           onViewportChange={(viewport) => {
-            setViewport({ ...viewport })
+            setViewport({ ...viewport });
           }}
         >
           {/* Markers */}
@@ -210,16 +182,12 @@ const [suggestedCards, setSuggestedCards] = useState([])
             style={geolocateControlStyle}
             positionOptions={{ enableHighAccuracy: true }}
             trackUserLocation={true}
-            fitBoundsOptions= {{linear: true, maxZoom: 3}}
+            fitBoundsOptions={{ linear: true, maxZoom: 3 }}
             auto
           />
-
         </ReactMapGL>
       </div>
-
     </div>
-
-
-  )
+  );
 }
-export default withRouter(MiniMap)
+export default withRouter(MiniMap);
