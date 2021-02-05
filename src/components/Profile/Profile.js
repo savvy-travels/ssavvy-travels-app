c900423ad3609e039063df1d11605c78fb4944b1
Nathan Waite <natew821@gmail.com>
2021-02-05 08:40:07 -0700

    Fixed Profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -8,25 +8,23 @@ import Header from "../Landing/Header/Header";
 const photos = require("../../photos.json");
 
 const Profile = (props) => {
-    const context = useContext(Context)
-    const [locations, setLocations] = useState([])
-    const [email, setEmail] = useState('')
-    const [preferred, setPreferred] = useState('')
+  const context = useContext(Context);
+  const [locations, setLocations] = useState([]);
+  const [email, setEmail] = useState("");
+  const [preferred, setPreferred] = useState("");
 
-    useEffect(() => {
-        axios.get('/api/auth/user')
-        .then((res) => {
-            setPreferred(res.data.preferred)
-            setEmail(res.data.email)
-        })
-    }, [])
-    
-    useEffect (()=> {
-        axios.get('/api/locations')
-        .then((res) => {
-            setLocations(res.data)
-        })
-    }, [])
+  useEffect(() => {
+    axios.get("/api/auth/user").then((res) => {
+      setPreferred(res.data.preferred);
+      setEmail(res.data.email);
+    });
+  }, []);
+
+  useEffect(() => {
+    axios.get("/api/locations").then((res) => {
+      setLocations(res.data);
+    });
+  }, []);
 
   const flights = context.quotes
     .map((quote) => {
@@ -43,36 +41,44 @@ const Profile = (props) => {
       };
     })
     .map((flight) => {
+      let airportId = context.allAirports.findIndex(
+        (airport) => airport.code == flight.IataCode
+      );
+      return { ...flight, ...context.allAirports[airportId] };
+    });
 
-      let airportId = context.allAirports.findIndex((airport) => airport.code == flight.IataCode)
-      return { ...flight, ...context.allAirports[airportId] }})
-    
-    const suggested = flights.slice(0, 10)
-    
-    const suggestedCards = suggested.map(flight => {
-        flight['photo'] = photos[Math.floor(Math.random() * photos.length)].url
-        return (
-        <div key={flight.QuoteId} className='profile-flight-card'>
-          <span className='profile-image-container'>
-            <img className='profile-card-image' src={flight.photo} alt='preview'/>
-          </span>
-          <span className='mini-info-container'>
-            <div className='mini-info-div'>
-              <h1>{flight.CityName}</h1>
-              <h4>{moment(flight.OutboundLeg.DepartureDate).format('MMM Do YYYY')}</h4>
-              {/* <h4>{`${flight.Direct ? 'Nonstop' : 'Multiple Stops'} - ${flight.name}`}</h4> */}
-              <h4>{flight.Name}</h4>
-              <button onClick={()=> context.goToCarrier(flight.Name)} className='book-button'>Book Flight</button>
-            </div>
-            <div className='mini-price'>
-              <h6 className='profile-from'>From</h6>
-              <h1> ${flight.MinPrice}</h1>
-            </div>
-            </span>
+  const suggested = flights.slice(0, 10);
+
+  const suggestedCards = suggested.map((flight) => {
+    flight["photo"] = photos[Math.floor(Math.random() * photos.length)].url;
+    return (
+      <div key={flight.QuoteId} className="profile-flight-card">
+        <span className="profile-image-container">
+          <img
+            className="profile-card-image"
+            src={flight.photo}
+            alt="preview"
+          />
+        </span>
+        <span className="mini-info-container">
+          <div className="mini-info-div">
+            <h1>{flight.CityName}</h1>
+            <h4>
+              {moment(flight.OutboundLeg.DepartureDate).format("MMM Do YYYY")}
+            </h4>
+            {/* <h4>{`${flight.Direct ? 'Nonstop' : 'Multiple Stops'} - ${flight.name}`}</h4> */}
+            <h4>{flight.Name}</h4>
+            <button
+              onClick={() => context.goToCarrier(flight.Name)}
+              className="book-button"
+            >
+              Book Flight
+            </button>
           </div>
-          <h1>
-            <h6>From</h6> ${flight.MinPrice}
-          </h1>
+          <div className="mini-price">
+            <h6 className="profile-from">From</h6>
+            <h1> ${flight.MinPrice}</h1>
+          </div>
         </span>
       </div>
     );
@@ -109,36 +115,11 @@ const Profile = (props) => {
   return (
     <div className="background-container">
       <Header />
-            <div className='profile-container'>
-                <div className='sidebar-container'>
-                <div className='user-container'>
-                    <div className='pro-container'>
-                    <h1>Profile</h1>
-                    </div>
-                    <h2><h2 className='email'>Email:</h2>{email}</h2>
-                    <h2><h2 className='pref'>Preferred Airport:</h2>{preferred}</h2>
-                </div>
-                <div className='suggested-container'>
-                    <Link to='/map'>
-                        <h3 className='links-profile'>View Map</h3>
-                    </Link>
-                    <Link to='/'>
-                        <h3 className='home'>Home</h3>
-                    </Link>
-                    <div className='line'></div>
-                    <h3>Suggested Trips</h3>
-                    {suggestedCards}
-                </div> 
-                </div>
-                <div className='locations-container'>
-                    <div className='input-container'>
-                        <h1>My Trips</h1>
-                        {/* <input className='search-trips-input' placeholder='Search My Trips'/> */}
-                    </div>
-                        <div className='locations-mapped'>
-                            {locationsMapped}
-                        </div>
-                </div>
+      <div className="profile-container">
+        <div className="sidebar-container">
+          <div className="user-container">
+            <div className="pro-container">
+              <h1>Profile</h1>
             </div>
             <h2>
               <h2 className="email">Email:</h2>
