3a7cee17c6a1f92afc7c8746cdc302f3e0f9baf0
Andrew Turner <turner.andrew0@gmail.com>
2021-02-06 22:13:03 -0600

    update

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,22 +1,21 @@
-import React, { useContext, useState } from "react"
-import { Switch, Route, withRouter } from "react-router-dom"
-import { airportSearch } from "../../Redux/searchReducer"
-import { connect } from "react-redux"
-import "./landing.css"
-import { Context } from "../../context/context"
-import NewSearch from "./NewSearch/NewSearch"
-import Signup from "./Auth/Signup"
-import Login from "./Auth/Login"
-import MiniMap from "./MiniMap/MiniMap"
-import Work from "./Works/Works"
-import CarouselComp from "./Carousel/Carousel"
-import { ClipLoader } from "react-spinners"
-import Header from "./Header/Header"
-require("dotenv").config()
-const allAirports = require('../airports.json')
+import React, { useContext, useState } from "react";
+import { Switch, Route, withRouter } from "react-router-dom";
+import { airportSearch } from "../../Redux/searchReducer";
+import { connect } from "react-redux";
+import "./landing.css";
+import { Context } from "../../context/context";
+import NewSearch from "./NewSearch/NewSearch";
+import Signup from "./Auth/Signup";
+import Login from "./Auth/Login";
+import MiniMap from "./MiniMap/MiniMap";
+import Work from "./Works/Works";
+import CarouselComp from "./Carousel/Carousel";
+import { ClipLoader } from "react-spinners";
+import Header from "./Header/Header";
+require("dotenv").config();
 
 function Landing(props) {
-  const context = useContext(Context)
+  const context = useContext(Context);
 
   // //About Project Modal State and Functions//
   // const [modal, setModal] = useState(false);
@@ -25,37 +24,39 @@ function Landing(props) {
   //   setModal(!modal);
   // };
 
+  console.log(context.allAirports)
+
   // Find Flights based off of your airport location
   const flights = context.quotes
     .map((quote) => {
       let destinationId = context.places.findIndex(
         (place) => place.PlaceId === quote.OutboundLeg.DestinationId
-      )
+      );
       let carrierId = context.carriers.findIndex(
         (carrier) => carrier.CarrierId === quote.OutboundLeg.CarrierIds[0]
-      )
+      );
       return {
         ...quote,
         ...context.places[destinationId],
         ...context.carriers[carrierId],
-      }
+      };
     })
     .map((flight) => {
-      let airportId = allAirports.findIndex(
+      let airportId = context.allAirports.findIndex(
         (airport) => airport.code.toLowerCase === flight.IataCode.toLowerCase
-      )
-      return { ...flight, ...context.allAirports[airportId] }
-    })
+      );
+      return { ...flight, ...context.allAirports[airportId] };
+    });
 
   // console.log(context.carriers);
 
   return (
-    <div className='landing'>
+    <div className="landing">
       <Header />
       <video
-        className='video'
-        src='https://colab-image-assets.s3-us-west-1.amazonaws.com/DevMtn-Air-New.mp4'
-        type='video/mp4'
+        className="video"
+        src="https://colab-image-assets.s3-us-west-1.amazonaws.com/DevMtn-Air-New.mp4"
+        type="video/mp4"
         autoPlay
         loop
         muted
@@ -65,23 +66,23 @@ function Landing(props) {
 
       {!context.loading ? (
         <Switch>
-          <Route exact path='/' component={NewSearch} />
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/signup' component={Signup} />
+          <Route exact path="/" component={NewSearch} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
         </Switch>
       ) : (
-        <div className='landing-welcome-message'>
+        <div className="landing-welcome-message">
           <h1>Welcome to Savvy Travels</h1>
           <img
-            src='https://colab-image-assets.s3-us-west-1.amazonaws.com/Savvy-Travels-logo.png'
-            alt='logo'
+            src="https://colab-image-assets.s3-us-west-1.amazonaws.com/Savvy-Travels-logo.png"
+            alt="logo"
           />
         </div>
       )}
 
       {/* <div className='triangle'></div> */}
 
-      <div className='mini-map-div'>
+      <div className="mini-map-div">
         {!context.loading ? (
           <MiniMap
             wait={8000}
@@ -97,19 +98,19 @@ function Landing(props) {
           </>
         )}
       </div>
-      <div className='landing-line'></div>
+      <div className="landing-line"></div>
       <CarouselComp flights={flights} />
-      <div className='landing-line'></div>
+      <div className="landing-line"></div>
       <Work />
     </div>
-  )
+  );
 }
 
 function mapStateToProps(reduxState) {
   return {
     username: reduxState.username,
     preferred: reduxState.preferred,
-  }
+  };
 }
 
-export default withRouter(connect(mapStateToProps, { airportSearch })(Landing))
+export default withRouter(connect(mapStateToProps, { airportSearch })(Landing));
